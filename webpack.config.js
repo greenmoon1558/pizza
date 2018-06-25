8978809faa6e8d6b2fe70dcf0731d37c488aee61
JanGordienko <svitlana.zhydejkina@gmail.com>
2018-06-25 22:30:38 +0300

    heroku3

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,7 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new UglifyJsPlugin({
+    new UglifyJSPlugin({
       sourceMap: true,
       compress: {
         warnings: false
